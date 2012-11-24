c1a0258fc479b874395b9f8688ae072a0835ebb8
ElizarovEugene <korp@bk.ru>
2012-11-24 09:04:19 +0400

    Удалил мусор

--- trackers/nnm-club.ru.engine.php
+++ trackers/nnm-club.ru.engine.php
@@ -101,7 +101,6 @@ class nnmclub
 	{
 		$monthes = array("Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек");
 		$month = mb_substr($data, 3, 6);
-		echo $month.'<br>';
 		$date = preg_replace("/(\d\d)\s(\d\d)\s(\d\d\d\d)/", "$3-$2-$1",str_replace($month, str_pad(array_search($month, $monthes)+1, 2, 0, STR_PAD_LEFT), $data));
 		$date = date("Y-m-d H:i:s", strtotime($date));
 	
