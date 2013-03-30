7cff84141f2901d92221a1624dbb2de567267634
Alexander Puzynia <werwolf.by@gmail.com>
2013-03-30 12:09:00 +0300

    [!] Fix credential editing for [rutor.org].

--- js/user-func.js
+++ js/user-func.js
@@ -425,7 +425,7 @@ function del_user(id)
 function changefunc() 
 {
 	var select = document.getElementById("selectfunc");
-	var a = ['anidub.com', 'kinozal.tv', 'lostfilm.tv', 'nnm-club.ru', 'novafilm.tv', 'rutracker.org'];
+	var a = ['anidub.com', 'kinozal.tv', 'lostfilm.tv', 'nnm-club.ru', 'novafilm.tv', 'rutracker.org', 'rutor.org'];
 	for (var i = 0; i < a.length; i++)
 	{
 		var e = a[i];
