604d164e36d12c518f0724298663fab43b1e25c5
Vyacheslav Mezetskiy <root@ishoo.ru>
2014-10-09 18:48:40 +0400

    Small fix :)

--- class/Transmission.class.php
+++ class/Transmission.class.php
@@ -34,9 +34,7 @@ class Transmission
         #добавляем торрент в torrent-клиента
         $command = `transmission-remote $torrentAddress $opt -a '$file' -w '$pathToDownload'`;
         if ( ! preg_match('/responded\:\s\"success\"/', $command))
-            return 'add_fail';
-        elseif (preg_match('/Couldn\'t connect to server/', $command))
-            return 'connect_fail';
+            return FALSE;
         else
         {
             #получаем хэш раздачи
