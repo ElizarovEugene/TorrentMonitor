1666c7bdb47791fb6f7f8ae759928ba85ca1b151
Upitep <maxchernov@yandex.ru>
2014-11-08 19:29:19 +0400

    Добавил поддержку управления торрент-клиентом Transmission по протоколу RPC (с помощью класса PHP-Transmission-Class). Обращаю внимание, что оригинальный класс будет портить не ASCII-кодировку, т.к. применяется принудительная перекодировка в UTF-8. С минимальными изменениями логики исправил найденные несостыковки в System.class и Errors.class

--- class/Transmission.class.php
+++ class/Transmission.class.php
@@ -1,51 +1,67 @@
 <?php
+
+require_once( dirname( __FILE__ ) . '/TransmissionRPC.class.php' );
+
+
 class Transmission
 {
     #добавляем новую закачку в torrent-клиент, обновляем hash в базе
     public static function addNew($id, $file, $hash, $tracker)
     {
-        #получаем настройки из базы
-        $settings = Database::getAllSetting();
-        foreach ($settings as $row)
-        {
-        	extract($row);
-        }
-        $opt = '';
-        if ( ! empty($torrentLogin) && ! empty($torrentPassword))
-            $opt = '-n '.$torrentLogin.':'.$torrentPassword;
+    	try
+    	{
+    		#получаем настройки из базы
+        	$settings = Database::getAllSetting();
+        	foreach ($settings as $row)
+        	{
+        		extract($row);
+        	}
+        	if ( ! empty($torrentLogin) && ! empty($torrentPassword))
+           	 $rpc = new TransmissionRPC('http://'.$torrentAddress.'/transmission/rpc', $torrentLogin, $torrentPassword);
+           	 //$rpc->debug=true;
+           	 $result = $rpc->sstats();
 
-        $individualPath = Database::getTorrentDownloadPath($id);
-        if ( ! empty($individualPath))
-            $pathToDownload = $individualPath;
+        	$individualPath = Database::getTorrentDownloadPath($id);
+        	if ( ! empty($individualPath))
+	            $pathToDownload = $individualPath;
 
-        if ( ! empty($hash))
-        {
-            $delOpt = '-r';
-            if ($tracker == 'lostfilm.tv' || $tracker == 'novafilm.tv')
-            {
-                if ($deleteOldFiles)
-                    $delOpt = '--remove-and-delete';
-            }
+    	    if ( ! empty($hash))
+	        {
+    	        $delOpt = 'false';
+        	    if ($tracker == 'lostfilm.tv' || $tracker == 'novafilm.tv')
+            	{
+                	if ($deleteOldFiles)
+                   	$delOpt = 'true';
+	            }
             
-            #удяляем существующую закачку из torrent-клиента
-            $command = `transmission-remote $torrentAddress $opt -t $hash $delOpt`;
-        }
+    	        #удяляем существующую закачку из torrent-клиента
+        	     $result = $rpc->remove($hash, $delOpt);
+        	}
 
-        #добавляем торрент в torrent-клиента
-        $command = `transmission-remote $torrentAddress $opt -a '$file' -w '$pathToDownload'`;
-        if ( ! preg_match('/responded\:\s\"success\"/', $command))
-            return FALSE;
-        else
-        {
-            #получаем хэш раздачи
-            $hashNew = `transmission-show '$file' | grep Hash | awk '{print $2}'`;
-            #обновляем hash в базе
-            Database::updateHash($id, $hashNew);
-        
-            //сбрасываем варнинг
-            Database::clearWarnings('Transmission');
-            return TRUE;
-        }
+        	#добавляем торрент в torrent-клиент
+          	$result = $rpc->add('/mnt/Data/Files/torrents'.$file, $pathToDownload);
+          	$command = $result->result;
+          	$idt = $result->arguments->torrent_added->id;
+        	if (preg_match('/success/', $command))
+        	{
+        		#получаем хэш раздачи
+        		$result = $rpc->get($idt, array('hashString'));
+        		$hashNew = $result->arguments->torrents[0]->hashString;
+        		#обновляем hash в базе
+        		Database::updateHash($id, $hashNew);
+        		
+        		//сбрасываем варнинг
+        		Database::clearWarnings('Transmission');
+        		return 'success';
+        	}
+        	else
+        		return 'add_fail';
+    	}
+    	catch (Exception $e)
+    	{
+ //   		die('[ERROR] ' . $e->getMessage() . PHP_EOL);
+ 			return $e->getMessage();
+    	}
     }
 }
 ?>
