d07cd1f37b00fdb2f9b5c21b8a3ed52b48f72e20
Vladimir Maksimenko <vl.maksime@gmail.com>
2015-03-20 04:32:53 +0300

    Изменен алгоритм формирования RSS ленты
    В продолжение нашей сегодняшней (точнее уже вчерашней) беседы о RSS ленте переделал алгоритм формирования.
    Теперь выполняется сканирование папки "torrents" на наличие *.torrent файлов. Все найденные файлы выводятся в ленту, отсортированный в порядке убывания даты изменения.
    Для создания XML документа использую DomDocument. Это позволило избавиться от html вставок модуле.

--- rss/index.php
+++ rss/index.php
@@ -1,61 +1,83 @@
 <?php
 $dir = dirname(__FILE__).'/../';
 include_once $dir.'class/Database.class.php';
+
 $rss = Database::getSetting('rss');
-if ($rss)
-{
+
+if ($rss) {
+
     header('Content-type: text/xml');
-    echo '<?xml version="1.0" encoding="utf-8" ?>'; //if short_open_tag :(
-?>
-    <rss version="0.91">
-    <channel>
-    <title>TorrentMonitor RSS</title>
-    <?php
-    $url = $_SERVER["HTTP_HOST"].str_replace('/rss/index.php', '', $_SERVER["SCRIPT_NAME"]);
-    ?>
-    <link>http://<?php echo $url?>/rss/</link>
-    <lastBuildDate><?php echo date("r");?></lastBuildDate>
-    <language>ru</language>
-    <?php
-    $torrents_list = Database::getTorrentsList('dateDesc');
-    foreach($torrents_list as $row)
-    {
-        extract($row);
-        ?>
-    <item>
-        <title><?php echo $name?></title>
-        <pubDate><?php echo $timestamp?></pubDate>
-        <?php
-        $season = substr($ep, 1, 2);
-    	$episode = substr($ep, -2);
-    	if ($tracker == 'rutracker.org' || $tracker == 'nnm-club.me' || $tracker == 'rutor.org' || $tracker == 'tfile.me' || $tracker == 'kinozal.tv' || $tracker == 'anidub.com' || $tracker == 'casstudio.tv'  || $tracker == 'animelayer.ru' || $tracker == 'tracker.0day.kiev.ua' || $tracker == 'torrents.net.ua' || $tracker == 'pornolab.net' || $tracker == 'rustorka.com')
-    	{
-    	    $name = $torrent_id;
-    	    $amp = NULL;
-    	    $app = NULL;
-        }
-        else
-        {
-            $name = $name;
-            if ($hd == 1 || $hd == 3)
-        		$amp = 'HD';
-        	elseif ($hd == 2)
-        		$amp = 'MP4';
-        	else
-        		$amp = NULL;
-            $app = '.S'.$season.'E'.$episode.'.';
-        }
-        $file = str_replace(' ', '.', $name).$app.$amp;
-        $link = 'http://'.$url.'/torrents/['.$tracker.']_'.$file.'.torrent';
-        ?>
-        <link><?php echo $link?></link>
-    </item>
-    <?php
+
+    $url = 'http://'.$_SERVER["HTTP_HOST"].str_replace('/rss/index.php', '', $_SERVER["SCRIPT_NAME"]);
+
+    $xml=new DomDocument('1.0','utf-8'); // создаем XML документ
+    $xml->formatOutput = true;           // включаем форматирование документа
+
+    // формируем корневой элемент ('rss')
+    $root = $xml->appendChild($xml->createElement('rss'));
+    $root->setAttribute('version','0.91');
+
+    // формируем элемент 'channel'
+    $channel = $root->appendChild($xml->createElement('channel'));
+
+    ////////////////////////////////////
+    // Заполняем заголовок документа
+
+    // формируем элемент 'title'
+    $title = $channel->appendChild($xml->createElement('title'));
+    $title->appendChild($xml->createTextNode('TorrentMonitor RSS'));
+
+    // формируем элемент 'link'
+    $link = $channel->appendChild($xml->createElement('link'));
+    $link->appendChild($xml->createTextNode($url.'/rss/'));
+
+    // формируем элемент 'lastBuildDate'
+    $lastBuildDate = $channel->appendChild($xml->createElement('lastBuildDate'));
+    $lastBuildDate->appendChild($xml->createTextNode(date("r")));
+
+    // формируем элемент 'language'
+    $language = $channel->appendChild($xml->createElement('language'));
+    $language->appendChild($xml->createTextNode('ru'));
+
+    ////////////////////////////////////////////////////////////////////////
+    // Подготавливаем список *torrent файлов для заполнения тела докумена
+
+    // формируем ассоциативный массив, где ключем выступает имя файла, а значением время изменения
+    $torrentsList = array();
+    foreach (glob("$dir/torrents/*.torrent") as $torrentFile) {
+        $fileName = basename($torrentFile);
+        $timestam = filemtime($torrentFile);
+        
+        $torrentsList[$fileName] = $timestam;
     }
-    ?>
-    </channel>
-    </rss>
-<?php
+
+    // выполняем сортировку массива в порядке убывания значения
+    arsort($torrentsList);
+
+    ////////////////////////////////////
+    // Заполняем тело документа
+
+    // делаем обход по элементам массива
+    foreach ($torrentsList as $fileName => $fileDate) {
+
+        // формируем элемент 'item'
+        $item = $channel->appendChild($xml->createElement('item'));
+
+        // формируем элемент 'title'
+        $title = $item->appendChild($xml->createElement('title'));
+        $title->appendChild($xml->createTextNode($fileName));
+
+        // формируем элемент 'pubDate'
+        $pubDate = $item->appendChild($xml->createElement('pubDate'));
+        $pubDate->appendChild($xml->createTextNode(date("r", $fileDate)));
+
+        // формируем элемент 'link'
+        $link = $item->appendChild($xml->createElement('link'));
+        $link->appendChild($xml->createTextNode($url.'/torrents/'.$fileName));
+    } 
+
+    // выводим содержимое XML документа
+    print $xml->saveXML();
 }
 else
     echo '<h1>RSS disabled</h1>';
