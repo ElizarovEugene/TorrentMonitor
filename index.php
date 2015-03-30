983a4d5cf9c0e5db40f92ea0f5a65d57723be3f3
Vladimir Maksimenko <vl.maksime@gmail.com>
2015-03-31 00:58:16 +0300

    index.php
    забыл обновить index.php

--- index.php
+++ index.php
@@ -1,14 +1,41 @@
 <?php
+define('ROOT_DIR', dirname(__FILE__).'/' );
+
 session_start();
 
-$dir = dirname(__FILE__)."/";
-include_once $dir."config.php";
-include_once $dir."class/Database.class.php";
-include_once $dir."class/System.class.php";
+include_once ROOT_DIR."config.php";
+include_once ROOT_DIR."class/System.class.php";
+include_once ROOT_DIR."class/Database.class.php";
+include_once ROOT_DIR."class/rain.tpl.class.php";
+
+// заполнение шаблона
+raintpl::configure("root_dir", ROOT_DIR );
+raintpl::configure("tpl_dir" , Sys::getTemplateDir() );
 
 if (Sys::checkAuth())
-    require_once "pages/main.php";
+{
+    $errors = Database::getWarningsCount();
+    
+    $count = 0;
+    if ( ! empty($errors))
+        for ($i=0; $i<count($errors); $i++)
+            $count += $errors[$i]['count'];
+    
+    $tpl = new RainTPL;
+    $tpl->assign( "update"     , Sys::checkUpdate() );
+    $tpl->assign( "version"    , Sys::version() );
+    $tpl->assign( "error_count", $count );
+
+    $content = $tpl->draw( 'main', true );
+}
 else
-    require_once "pages/auth.php";
+{
+    $tpl = new RainTPL;
+    $content = $tpl->draw( 'auth', true );
+}
+
+$tpl = new RainTPL;
+$tpl->assign( "content", $content );
+$tpl->draw( "index" );
 
 ?>
